34971babe597f21c196a0108979740acaa4ec1cb
Edward Leung <edward_leung_12@hotmail.com>
2020-07-30 21:15:41 +0100

    update

--- main.js
+++ main.js
@@ -14,6 +14,37 @@ const starsTotal = 5;
 // Run getRatings when DOM loads
 document.addEventListener("DOMContentLoaded", getRatings);
 
+// Form Elements
+const productSelect = document.getElementById("product-select");
+const ratingControl = document.getElementById("rating-control");
+
+//Init product
+let product;
+
+// Product select change
+productSelect.addEventListener("change", (e) => {
+  product = e.target.value;
+  // Enable rating control
+  ratingControl.disabled = false;
+  ratingControl.value = ratings[product];
+});
+
+// Rating Control Change
+ratingControl.addEventListener("blur", (e) => {
+  const rating = e.target.value;
+
+  // Make sure between 5 or under
+  if (rating > 5) {
+    alert("Please rate 1 - 5");
+    return;
+  }
+
+  // Change rating
+  ratings[product] = rating;
+
+  getRatings();
+});
+
 // Get ratings
 function getRatings() {
   // Loop through our object
@@ -27,5 +58,9 @@ function getRatings() {
     // Set width of stars-inner to percentage
     document.querySelector(`.${rating}
     .stars-inner`).style.width = starPercentageRounded;
+
+    // Add number rating
+    document.querySelector(`.${rating}
+    .number-rating`).innerHTML = ratings[rating];
   }
 }
